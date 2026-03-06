aecf97458d0dc1f597dec21cfb0b75a23887a941
santizafer <santizafer@gmail.com>
2026-03-05 23:20:16 -0500

    first commit

---
+++ robots.py
@@ -0,0 +1,9 @@
+import requests
+
+url = 'https://elcomercio.pe/robots.txt'
+response = requests.get(url)
+
+if response.status_code == 200:
+  print("robots.txt encontrado")
+else:
+  print("robots.txt no encontrado")
